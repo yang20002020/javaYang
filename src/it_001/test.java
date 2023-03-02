8a2de4b5943e3491d19c4bd78bb45603b78b37e9
26286 <2628636987@qq.com>
2023-03-02 22:26:43 +0800

    scanner

---
+++ src/it_001/test.java
@@ -0,0 +1,16 @@
+package it_001;
+
+import java.util.Scanner;
+
+public class test {
+    public  static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        //接受数据
+        int x=sc.nextInt();
+        //输出
+        System.out.println(x);
+    }
+}
+
+
+
